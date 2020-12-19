8077beb0f12882a648fa4b930d1a9a62772b6849
Sylwia Marek <sylwia1301@onet.pl>
2020-12-20 00:36:16 +0100

    added animal unit tests

--- src/test/agh/cs/project1/AnimalTest.java
+++ src/test/agh/cs/project1/AnimalTest.java
@@ -1,9 +1,13 @@
 package agh.cs.project1;
 
 import agh.cs.project1.simulation.Animal;
+import agh.cs.project1.simulation.MapDirection;
 import agh.cs.project1.simulation.Savannah;
 import agh.cs.project1.simulation.Vector2d;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static agh.cs.project1.simulation.MapDirection.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,5 +44,68 @@ class AnimalTest {
         assertEquals(8,animal.getEnergy());
     }
 
+    @Test
+    public void setColorTest(){
+        Savannah map = new Savannah(3,3,5);
+        Animal animal = new Animal(map,new Vector2d(0,0), SOUTH,6,0);
 
+        Color green = new Color(36, 141, 75);
+        Color orange = new Color(219, 145, 28);
+        Color red = new Color(210, 10, 35);
+
+        animal.move(1);
+        animal.move(1);
+        assertEquals(animal.setColor(), green);
+        animal.move(1);
+        assertEquals(animal.setColor(), orange);
+        animal.move(1);
+        animal.move(1);
+        assertEquals(animal.setColor(), red);
+
+    }
+
+    @Test
+    public void getOrientationTest(){
+        Savannah map = new Savannah(300,300,5);
+        Animal animal = new Animal(map,new Vector2d(10,99), SOUTH,8,0);
+
+        assertEquals(animal.getOrientation(), SOUTH);
+        animal.move(2);
+        assertEquals(animal.getOrientation(), WEST);
+    }
+
+    @Test
+    public void getPositionTest(){
+        Savannah map = new Savannah(300,300,5);
+        Animal animal = new Animal(map,new Vector2d(10,99), SOUTH,8,0);
+
+        assertEquals(animal.getPosition(),new Vector2d(10,99));
+        animal.move(2);
+        assertEquals(animal.getPosition(),new Vector2d(9,99));
+    }
+
+    @Test
+    public void chooseTurnTest(){
+        Savannah map = new Savannah(2,2,5);
+        Animal animal = new Animal(map,new Vector2d(0,1), NORTH_EAST,8,0);
+        int turn = animal.chooseTurn();
+        assertTrue(turn >= 0 && turn < 8);
+    }
+
+    @Test
+    public void reproduceTest(){
+        Savannah map = new Savannah(5,4,5);
+        Animal animal1 = new Animal(map,new Vector2d(2,2), NORTH_EAST,8,0);
+        Animal animal2 = new Animal(map,new Vector2d(2,2), SOUTH,3,0);
+
+        // child should be placed around parents
+        Animal child = animal1.reproduce(animal2,2);
+        Vector2d childPosition = child.getPosition();
+        assertTrue(childPosition.x >= 1 && childPosition.x <= 3);
+        assertTrue(childPosition.y >= 1 && childPosition.y <= 3);
+        assertTrue(childPosition.x != 2 || childPosition.y != 2);
+
+        // animal get 1/4 energy from both parents
+        assertEquals(child.getEnergy(), 2);
+    }
 }
